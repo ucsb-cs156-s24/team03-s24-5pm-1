7d661854c31695affe86bbf60c705bec22c1c3f1
Rick T. Zhang <rtzhang@ucsb.edu>
2024-05-08 00:58:00 +0000

    rt.z - Created ucsbDiningCommonsMenuItemFixures.js

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDatesFixtures = {
+    oneItem: {
+        "id": 1,
+        "diningCommonsCode": "ortega",
+        "name": "Baked Pesto Pasta with Chicken",
+        "station": "Entrees"
+    },
+    threeItems: [
+        {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "Baked Pesto Pasta with Chicken",
+            "station": "Entrees"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "portola",
+            "name": "Grilled Chicken Taco",
+            "station": "Chef's Choice"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "thewar",
+            "name": "Buttermilk Fried Chicken",
+            "station": "Blue Plate Special"
+        }
+    ]
+};
+
+
+export { ucsbDatesFixtures };
