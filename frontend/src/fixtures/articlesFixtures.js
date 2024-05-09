b7f380d6f422e1f91a8c8c99dfa63ac378ae8b72
Crossover661 <sanjay.k.srikanth@gmail.com>
2024-05-09 14:36:27 -0700

    push updates for articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "My Article",
+        "url": "myarticle.qwe",
+        "explanation": "An article used for testing",
+        "email": "sample@email.xy",
+        "dateAdded": "2011-11-11T11:11:11"
+    },
+    threeArticles: [
+        {
+            "id": 2,
+            "title": "My First Article",
+            "url": "myfirstarticle.qwe",
+            "explanation": "TestArticle1",
+            "email": "article1@x.yz",
+            "dateAdded": "2012-03-04T05:06:07"
+        },
+        {
+            "id": 3,
+            "title": "My Second Article",
+            "url": "mysecondarticle.qwe",
+            "explanation": "TestArticle2",
+            "email": "article2@x.yz",
+            "dateAdded": "2019-11-14T07:38:00"
+        },
+        {
+            "id": 4,
+            "title": "My Third Article",
+            "url": "mythirdarticle.qwe",
+            "explanation": "TestArticle3",
+            "email": "article3@x.yz",
+            "dateAdded": "2023-12-19T00:01:02"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
