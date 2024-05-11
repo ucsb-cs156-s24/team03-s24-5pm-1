6de71bc58746cd5770be43ebdc237c3f53da5e7a
Rick T. Zhang <rtzhang@ucsb.edu>
2024-05-11 20:41:04 +0000

    rt.z - Added contents to UCSB Dining Commons Menu Item Edit Page

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage.js
@@ -1,12 +1,68 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import UCSBMenuItemForm from "main/components/UCSBDiningCommonsMenuItem/UCSBMenuItemForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function UCSBDiningCommonsMenuItemEditPage() {
+export default function UCSBDiningCommonsMenuItemEditPage({storybook=false}) {
+  let { id } = useParams();
 
-  // Stryker disable all : placeholder for future implementation
+  const { data: ucsbMenuItem, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/ucsbmenuitems?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/ucsbmenuitems`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (ucsbMenuItem) => ({
+    url: "/api/ucsbmenuitems",
+    method: "PUT",
+    params: {
+      id: ucsbMenuItem.id,
+    },
+    data: {
+      quarterYYYYQ: ucsbMenuItem.diningCommonCode,
+      name: ucsbMenuItem.name,
+      station: ucsbMenuItem.station
+    }
+  });
+
+  const onSuccess = (ucsbMenuItem) => {
+    toast(`UCSBDiningCommonsMenuItem Updated - id: ${ucsbMenuItem.id} name: ${ucsbMenuItem.name}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/ucsbmenuitems?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/ucsbmenuitems" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit UCSBDiningCommonsMenuItem</h1>
+        {
+          ucsbMenuItem && <UCSBMenuItemForm initialContents={ucsbMenuItem} submitAction={onSubmit} buttonLabel="Update" />
+        }
       </div>
     </BasicLayout>
   )
