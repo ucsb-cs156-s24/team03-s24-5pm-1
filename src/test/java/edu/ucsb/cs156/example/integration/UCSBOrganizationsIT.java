7e6e6f11595db355ff5217ea8808f8f6429bd3d0
ylin250 <ylin250@ucsb.edu>
2024-06-02 13:10:03 -0700

    fix7

--- src/test/java/edu/ucsb/cs156/example/integration/UCSBOrganizationsIT.java
+++ src/test/java/edu/ucsb/cs156/example/integration/UCSBOrganizationsIT.java
@@ -88,14 +88,12 @@ public class UCSBOrganizationsIT {
                                 .orgCode("SKY")
                                 .orgTranslationShort("SKYDIVING_CLUB")
                                 .orgTranslation	("SKYDIVING_CLUB_AT_UCSB")
-                                .inactive(false)
+                                .inactive(true)
                                 .build();
-                
-                when(ucsbOrganizationsRepository.save(eq(org1))).thenReturn(org1);
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                post("/api/UCSBOrganizations/post?orgCode=SKY&orgTranslationShort=SKYDIVING_CLUB&orgTranslation=SKYDIVING_CLUB_AT_UCSB&inactive=false")
+                                post("/api/UCSBOrganizations/post?orgCode=SKY&orgTranslationShort=SKYDIVING_CLUB&orgTranslation=SKYDIVING_CLUB_AT_UCSB&inactive=true")
                                                 .with(csrf()))
                                 .andExpect(status().isOk()).andReturn();
 
